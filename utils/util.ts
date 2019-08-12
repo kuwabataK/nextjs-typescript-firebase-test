53917f691de3af93db237f7757d870b4bd5bfa49
KuwabataK <>
2019-08-13 01:11:35 +0900

    enhance arrayToObject

--- utils/util.ts
+++ utils/util.ts
@@ -1,20 +1,29 @@
 
 /**
- * 配列を指定したキーのMap Objectに変換します。
+ * 配列を指定したキーのMap Objectに変換する
  * 
  * @param array 
- * @param key 
+ * @param {string} key 変換後の連想配列のキーに指定する値のキーを指定する。例 'id' / 'innerItem.name'
  */
-export function arrayToMap<T extends MapObj>(array: T[], key: string) {
-
+export function arrayToMap<T extends MapObj | string | number>(array: T[], key: string = '') {
     const obj: { [key: string]: T } = {}
-    array.forEach(t => {
-        if (key in t) {
-            const _key: string = t[key]
-            obj[_key] = t
+
+    const re = /[\.\[\]]/
+    const keys = key.split(re).filter(str => str.length !== 0)
+    
+    array.forEach((t, i) => {
+        if (typeof t === 'string' || typeof t === 'number') {
+            obj[t] = t
+            return
         }
+        const _key = keys.reduce((acc, cur) => acc[cur], t as MapObj)
+        if (typeof _key !== 'string' && typeof _key !== 'number') {
+            throw new Error(`array[${i}].${key} is ${typeof _key}. not string or number !!`)
+        }
+        obj[_key] = t
     })
     return obj
 }
+
 
 type MapObj = { [key: string]: any }
