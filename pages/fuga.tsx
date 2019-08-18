0211884c5ba49a8f711299645063d2637f747a65
KuwabataK <>
2019-08-18 20:30:48 +0900

    change class component to functional component

--- pages/fuga.tsx
+++ pages/fuga.tsx
@@ -1,69 +1,70 @@
-import * as React from 'react'
 import Layout from '../components/Layout'
 import { childStore } from '../store/store'
 import { observer } from 'mobx-react'
 import { Fuga } from '../store/ChildStore';
+import { useState, useEffect } from 'react';
 
 
-@observer
-class FugaPage extends React.Component {
+const FugaPage: React.FunctionComponent = observer(() => {
 
-    state = {
-        name: ''
-    }
+    const [name, setName] = useState()
 
-    addFuga = () => {
+    useEffect(() => {
+        console.log('fugaが変更されました')
+    }, [childStore.fuga])
+
+    const addFuga = () => {
         const newFuga = new Fuga()
-        newFuga.name = this.state.name
+        newFuga.name = name
         newFuga.isActive = true
         childStore.addFuga(newFuga)
     }
 
-    addDisableFuga = () => {
+    const addDisableFuga = () => {
         const newFuga = new Fuga()
-        newFuga.name = this.state.name
+        newFuga.name = name
         newFuga.isActive = false
         childStore.addFuga(newFuga)
     }
 
-    onChangeInput = (e: any) => {
-        this.setState({ name: e.target.value })
+    const onChangeInput = (e: any) => {
+        setName(e.target.value)
     }
 
-    filtered = () => {
+    const filtered = () => {
         childStore.filteredFuga()
     }
 
-    changeName = () => {
+    const changeName = () => {
         childStore.changeFirstFugaName()
     }
 
-    changeState = (id: string) => {
+    const changeState = (id: string) => {
         childStore.changeState(id)
     }
 
-    render() {
-        return (
-            <Layout>
-                {[...childStore.fuga.values()].map(f => (
-                    <div key={f.id}>
-                        <p>
-                            {f.name}
-                        </p>
-                        <p>
-                            {f.address.states}
-                        </p>
-                        <button onClick={() => this.changeState(f.id)}>国を変える</button>
-                    </div>
-                ))}
-                <input value={this.state.name} onChange={this.onChangeInput}></input>
-                <button onClick={this.addFuga}>add</button>
-                <button onClick={this.addDisableFuga}>addDisable</button>
-                <button onClick={this.filtered}>deleteDisable</button>
-                <button onClick={this.changeName}>change First Name</button>
-            </Layout>
-        )
-    }
-}
+
+    return (
+        <Layout>
+            {[...childStore.fuga.values()].map(f => (
+                <div key={f.id}>
+                    <p>
+                        {f.name}
+                    </p>
+                    <p>
+                        {f.address.states}
+                    </p>
+                    <button onClick={() => changeState(f.id)}>国を変える</button>
+                </div>
+            ))}
+            <input value={name} onChange={onChangeInput}></input>
+            <button onClick={addFuga}>add</button>
+            <button onClick={addDisableFuga}>addDisable</button>
+            <button onClick={filtered}>deleteDisable</button>
+            <button onClick={changeName}>change First Name</button>
+        </Layout>
+    )
+
+})
 
 export default FugaPage
